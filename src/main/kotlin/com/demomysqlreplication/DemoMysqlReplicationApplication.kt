d7d1c4cc361817cbf5c6da6ffac617f98486c565
han <tkrhktkdwk10@gmail.com>
2025-03-04 10:35:13 +0900

    Inital

---
+++ src/main/kotlin/com/demomysqlreplication/DemoMysqlReplicationApplication.kt
@@ -0,0 +1,11 @@
+package com.demomysqlreplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoMysqlReplicationApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoMysqlReplicationApplication>(*args)
+}
---
+++ src/test/kotlin/com/demomysqlreplication/DemoMysqlReplicationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demomysqlreplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoMysqlReplicationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
